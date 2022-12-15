dae1b392d5ccd0964a9825d752aa0f4d6f5c8841
Zhuk7777 <93807982+Zhuk7777@users.noreply.github.com>
2022-12-15 15:35:18 +0300

    zbs

--- Chm2/Main.cpp
+++ Chm2/Main.cpp
@@ -1,6 +1,8 @@
 #include"System.h"
 
 double* generateVector(int size, double max, double min);
+double normOfVec(double* vec, int size);
+double* differenceVec(double* vec1, double* vec2, int size);
 
 int main()
 {
@@ -25,12 +27,17 @@ int main()
 	std::cout << "Точное решение\n";
 	for (int i = 0; i < n; i++)
 		std::cout << x[i] << " ";
-	std::cout << "\n";
+	std::cout << "\n\n";
 
 	std::cout << "Полученное решение\n";
 	double* result = s.systemSolution();
 	for (int i = 0; i < n; i++)
 		std::cout << result[i] << " ";
+	std::cout << "\n\n";
+
+	std::cout << "Погрешность\n";
+	result = differenceVec(x, result, n);
+	std::cout << normOfVec(result, n);
 
 
 	return 0;
@@ -44,4 +51,22 @@ double* generateVector(int size, double max, double min) {
 			x[j] = x[j] + abs(rand() % (int)max / 3 + min);
 	}
 	return x;
+}
+
+double normOfVec(double* vec, int size)
+{
+	double max = abs(vec[0]);
+	for (int i = 1; i < size; i++)
+		if (abs(vec[i]) > max)
+			max = abs(vec[i]);
+
+	return max;
+}
+
+double* differenceVec(double* vec1,double* vec2,int size)
+{
+	double* result = new double[size];
+	for (int i = 0; i < size; i++)
+		result[i] = vec2[i] - vec1[i];
+	return result;
 }
